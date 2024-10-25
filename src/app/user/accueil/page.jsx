ab368f6cfc6d061a78d20b95fb8b9034a217b7c7
landou <landoumechack@gmail.com>
2024-10-25 18:43:57 +0100

    page

---
+++ src/app/user/accueil/page.jsx
@@ -0,0 +1,16 @@
+import Accueil from "@/app/components/accueil";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+
+
+import home from "@/app/page";
+
+export default home = () => {
+    return(
+        <>
+        <Header/>
+        <Accueil/>
+        <Footer/>
+        </>
+    );
+};
---
+++ src/app/user/verification/page.jsx
@@ -0,0 +1,14 @@
+import Verification from "@/app/components/verification";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+
+
+export default verifier = () => {
+    return(
+        <>
+        <Header/>
+        <Verification/>
+        <Footer/>
+        </>
+    );
+};
